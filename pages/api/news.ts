74b6a053e1d53c35d4ed934873a88804f0dfcf84
sor4chi <sorakawa0218@gmail.com>
2022-04-02 11:05:29 +0900

    Add: Create API Route of News

---
+++ pages/api/news.ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "lib/prisma";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> {
+  res.statusCode = 200;
+  const news = await prisma.news.findMany();
+  res.json(news);
+}
