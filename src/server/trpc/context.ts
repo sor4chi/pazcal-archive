87fa568e6bdd4ec1f26f414de813549cb2389bf1
kawamurasorachi <sorakawa0218@gmail.com>
2022-10-26 15:07:56 +0900

    feat: add lintfix command

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,9 +1,10 @@
 // src/server/router/context.ts
+import { getServerAuthSession } from "../common/get-server-auth-session";
+import { prisma } from "../db/client";
+
 import type { inferAsyncReturnType } from "@trpc/server";
 import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
 import type { Session } from "next-auth";
-import { getServerAuthSession } from "../common/get-server-auth-session";
-import { prisma } from "../db/client";
 
 type CreateContextOptions = {
   session: Session | null;
--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,7 +1,8 @@
 // src/server/trpc/router/_app.ts
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
+
 import { authRouter } from "./auth";
+import { exampleRouter } from "./example";
 
 export const appRouter = router({
   example: exampleRouter,
